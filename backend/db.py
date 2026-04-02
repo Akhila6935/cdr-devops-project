71fabbe5097211b7fbb7d4d75249d78c3faeb987
Akhila Vodnala <169081546+Akhila6935@users.noreply.github.com>
2026-04-02 19:20:28 +0530

    Update database connection parameters

--- backend/db.py
+++ backend/db.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="mysql.connector",
         user="root",
-        password="password",
+        password="root",
         database="cdr_db"
     )
